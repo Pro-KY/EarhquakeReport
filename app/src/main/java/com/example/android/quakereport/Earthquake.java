7d4e59c4e65a522f54128d0eb0e40dd89f1b0d58
pro-KY <polystar66@gmail.com>
2017-03-09 17:03:43 +0200

    design: split location and date into 2 TextViews

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,9 +1,14 @@
 package com.example.android.quakereport;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Earthquake {
     private String mMagnitude;
     private String mLocation;
     private Long mDate;
+    //Locale ukraineLocale = new Locale("ukr", "ua");
 
     public Earthquake(String magnitude, String city, Long date) {
         mMagnitude = magnitude;
@@ -21,5 +26,17 @@ public class Earthquake {
 
     public String getMagnitude() {
         return mMagnitude;
+    }
+
+    // Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+    public String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy", Locale.US);
+        return dateFormat.format(dateObject);
+    }
+
+    // Return the formatted date string (i.e. "4:30 PM") from a Date object.
+    public String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a", Locale.US);
+        return timeFormat.format(dateObject);
     }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -4,6 +4,7 @@ package com.example.android.quakereport;
 import android.content.Context;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
+
+import static com.example.android.quakereport.R.id.place_text_view;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
@@ -42,23 +45,38 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude_text_view);
         magnitudeTextView.setText(currentEarthquake.getMagnitude());
 
-        // Find the TextView in the list_item.xml layout with the ID location_text_view
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location_text_view);
-        locationTextView.setText(currentEarthquake.getLocation());
+        // get earthquake location from an object
+        String location = currentEarthquake.getLocation();
+
+        TextView distanceTextView = (TextView) listItemView.findViewById(R.id.distance_text_view);
+        TextView placeTextView = (TextView) listItemView.findViewById(place_text_view);
+
+        // if there is distance in location string, then set distance and place of the earthquake into
+        // separate TextViews, otherwise display only place value
+        if(location.lastIndexOf("of") != -1) {
+            String distanceSubstring = location.substring(0, location.lastIndexOf("of")+2);
+            Log.d("topStringValue", distanceSubstring);
+            distanceTextView.setText(distanceSubstring);
+
+            String placeSubstring = location.substring(location.lastIndexOf("of")+3);
+            Log.d("bottomStringValue", placeSubstring);
+            placeTextView.setText(placeSubstring);
+        } else {
+            distanceTextView.setText(location);
+            placeTextView.setVisibility(View.GONE);
+        }
 
         // Find the TextView in the list_item.xml layout with the ID date_text_view
         TextView dateTextView = (TextView) listItemView.findViewById(R.id.date_text_view);
+        TextView timeTextView = (TextView) listItemView.findViewById(R.id.time_text_view);
 
         // Format date in milliseconds into human readable format
         long dateInMilliSeconds = currentEarthquake.getDate();
         Date dateObject = new Date(dateInMilliSeconds);
 
-        //Locale ukraineLocale = new Locale("ukr", "ua");
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy", Locale.US);
-        String dateToDisplay = dateFormatter.format(dateObject);
-
-        // set the date into dateTextView
-        dateTextView.setText(dateToDisplay);
+        // set date and time into appropriate TextViews
+        dateTextView.setText(currentEarthquake.formatDate(dateObject));
+        timeTextView.setText(currentEarthquake.formatTime(dateObject));
 
         return listItemView;
     }
