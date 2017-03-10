e261fda751737d3609c102fc39d280c7400eb3ec
Yuriy <klyuyko.yu@gmail.com>
2017-03-10 11:43:57 +0200

    Update EarthquakeAdapter.java

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -4,6 +4,7 @@ package com.example.android.quakereport;
 import android.content.Context;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,16 +43,40 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude_text_view);
         magnitudeTextView.setText(currentEarthquake.getMagnitude());
 
-        // Find the TextView in the list_item.xml layout with the ID location_text_view
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location_text_view);
-        locationTextView.setText(currentEarthquake.getLocation());
+        // get earthquake location from an object
+        String location = currentEarthquake.getLocation();
+
+        // displays the proximity of an earthquake
+        TextView proximityTextView = (TextView) listItemView.findViewById(R.id.proximity_text_view);
+        // displays place of the earthquake
+        TextView placeTextView = (TextView) listItemView.findViewById(R.id.place_text_view);
+
+        // if there is proximity in location string, then set distance and place of the earthquake into
+        // 2 separate TextViews, otherwise display "Near the" instead of proximity information
+        if(location.lastIndexOf("of") != -1) {
+            String distanceSubstring = location.substring(0, location.lastIndexOf("of")+2);
+            Log.d("topStringValue", distanceSubstring);
+            proximityTextView.setText(distanceSubstring);
+
+            String placeSubstring = location.substring(location.lastIndexOf("of")+3);
+            Log.d("bottomStringValue", placeSubstring);
+            placeTextView.setText(placeSubstring);
+        } else {
+            proximityTextView.setText("Near the");
+            placeTextView.setText(location);
+        }
 
         // Find the TextView in the list_item.xml layout with the ID date_text_view
         TextView dateTextView = (TextView) listItemView.findViewById(R.id.date_text_view);
+        TextView timeTextView = (TextView) listItemView.findViewById(R.id.time_text_view);
 
         // Format date in milliseconds into human readable format
         long dateInMilliSeconds = currentEarthquake.getDate();
         Date dateObject = new Date(dateInMilliSeconds);
+
+        // set date and time into appropriate TextViews
+        dateTextView.setText(currentEarthquake.formatDate(dateObject));
+        timeTextView.setText(currentEarthquake.formatTime(dateObject));
 
         //Locale ukraineLocale = new Locale("ukr", "ua");
         SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy", Locale.US);
